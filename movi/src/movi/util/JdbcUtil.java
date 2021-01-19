051e20431b206be63fbc9cc661d201dfb59a8c95
leeyerim <dpfla8628@gmail.com>
2021-01-19 21:31:46 +0900

    day1

--- movi/src/movi/util/JdbcUtil.java
+++ movi/src/movi/util/JdbcUtil.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package movi.util;
 
 import java.sql.Connection;
@@ -15,19 +14,3 @@ public class JdbcUtil {
 		//밖에서 JdbcUtil.getConnection("","")으로 불러 올 수 있게하자!
 	}
 }
-=======
-package movi.util;
-
-import java.sql.Connection;
-import java.sql.DriverManager;
-
-public class JdbcUtil {
-	
-	public static Connection getConnection(String username, String password) throws Exception {
-		Class.forName("oracle.jdbc.OracleDriver");
-		Connection con = DriverManager.getConnection(
-				"jdbc:oracle:thin:@localhost:1521:xe", username, password);
-		return con;
-	}
-}
->>>>>>> refs/remotes/origin/main
