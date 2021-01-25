2131b33b31977376b6386fcce307fd8b0fa7a555
par86 <par86@DESKTOP-6GU1ARN>
2021-01-25 21:16:04 +0900

    일단 없어진거 고친 듯?

--- movi/src/movi/beans/MemberFindDao.java
+++ movi/src/movi/beans/MemberFindDao.java
@@ -22,35 +22,7 @@ public class MemberFindDao {
 		ps.execute();
 		
 		con.close();
-	}
-	
-=======
-package movi.beans;
-
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-
-import movi.util.JdbcUtil;
-
-public class MemberFindDao {
-	
-	public static final String USERNAME = "movi";
-	public static final String PASSWORD = "movi";
-	
-	public String insert(MemberFindDto dto) throws Exception {
-		Connection con = JdbcUtil.getConnection(USERNAME, PASSWORD);
-		String sql = "insert into member("
-				+ "member_no,member_id,member_pw,member_nick,member_phone,member_auth)"
-				+ " values(member_seq.nextval,?,?,?,?,'일반')";
-		PreparedStatement ps = con.prepareStatement(sql);
-		ps.setString(1, dto.getMember_id());
-		ps.setString(2, dto.getMember_pw());
-		ps.setString(4, dto.getMember_phone());
-		ps.execute();
-		
-		con.close();
 		
 		return ""; /*여기에 뭘 리턴할지 써줘야 함*/
 	}
-	
 }
--- movi/src/movi/servlet/MemberLogoutServlet.java
+++ movi/src/movi/servlet/MemberLogoutServlet.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package movi.servlet;
 
 import java.io.IOException;
@@ -27,34 +26,4 @@ public class MemberLogoutServlet extends HttpServlet{
          resp.sendError(500);
       }
    }
-=======
-package movi.servlet;
-
-import java.io.IOException;
-
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-
-@WebServlet(urlPatterns = "/member/logout.do")
-public class MemberLogoutServlet extends HttpServlet{
-   @Override
-   protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-      try {
-         req.getSession().removeAttribute("check");
-         req.getSession().removeAttribute("auth");
-         req.getSession().invalidate();
-         
-           resp.sendRedirect("../index.jsp");
-   
-      }
-      catch(Exception e) {
-         e.printStackTrace();
-         resp.sendError(500);
-      }
-   }
->>>>>>> refs/remotes/origin/main
 }
